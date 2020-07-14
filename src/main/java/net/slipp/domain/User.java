899517a91aa09eaf64ebc4659505c6d32a117229
pbg0205 <pbg0205@naver.com>
2020-07-14 18:02:12 +0900

    개인정보 수정기능 구현완료.

---
+++ src/main/java/net/slipp/domain/User.java
@@ -0,0 +1,59 @@
+package net.slipp.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity //해당 class를 Entity(table)로 사용하겠다는 의미 
+public class User {
+	@Id //primary_key로 설정
+	@GeneratedValue //database에서 자동으로 값을 1씩 증가시키는 역할
+	private Long id;
+	
+	//(1) nullable = false : not null 설정 (2) length = 20 : id 문자열 길이
+	@Column(nullable = false, length = 20) 
+	private String userId;
+	private String password;
+	private String name;
+	private String email;
+	
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+	}
+
+	public void update(User updateUser) {
+		this.password = updateUser.password;
+		this.name = updateUser.name;
+		this.email = updateUser.email;
+		
+	}
+}
--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,28 +1,54 @@
 package net.slipp.web;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-	private List<User> users = new ArrayList<User>();
 	
-	@PostMapping("/create")
-	public String create(User user) {
-		System.out.println("user: " + user);
-		users.add(user);
-		return "redirect:/list";
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("/form")
+	public String form() {
+		return "user/form";
 	}
 	
-	@GetMapping("/list")
+	@PostMapping("")
+	public String create(User user) {
+		System.out.println("user: " + user);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+	
+	@GetMapping("") //조회창을 호출하는 메소드
 	public String list(Model model) {
-		model.addAttribute("users", users);
-		return "list";
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
+	}
+	
+	@GetMapping("{id}/form") //수정하고자 하는 정보를 호출하는 메소드
+	public String updateform(@PathVariable Long id, Model model) {
+		User user = userRepository.findById(id).get();
+		model.addAttribute("user", user);
+		return "user/updateform";
+	}
+	
+	@PostMapping("/{id}") //수정하고자  하는 id 정보를 호출하는 메소드
+	public String update(@PathVariable Long id, User updateUser) {
+		User user = userRepository.findById(id).get();
+		user.update(updateUser); //user class에서 수정하고 싶은 정보를 수정하는 메소드 
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 }
