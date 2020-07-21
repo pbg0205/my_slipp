88021bd02a8eb623b9ef3bf8e6007c441ac58869
pbg0205 <pbg0205@naver.com>
2020-07-21 16:46:16 +0900

    질문내용 수정 및 삭제 내용 구현 완료

--- src/main/java/net/slipp/web/QuestionController.java
+++ src/main/java/net/slipp/web/QuestionController.java
@@ -4,8 +4,12 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.slipp.domain.Question;
@@ -37,6 +41,32 @@ public class QuestionController {
 		Question newQuestion = new Question(sessionedUser, title, contents);
 		questionRepository.save(newQuestion);
 		
+		return "redirect:/";
+	}
+	
+	@GetMapping("/{id}")
+	public String show(@PathVariable Long id, Model model) {
+		model.addAttribute("question", questionRepository.findById(id).get());
+		return "/qna/show";
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		model.addAttribute("question", questionRepository.findById(id).get());
+		return "/qna/updateForm";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, String title, String contents) {
+		Question question = questionRepository.findById(id).get();
+		question.update(title, contents);
+		questionRepository.save(question);
+		return String.format("redirect:/questions/%d", id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable Long id) {
+		questionRepository.deleteById(id);
 		return "redirect:/";
 	}
 }
